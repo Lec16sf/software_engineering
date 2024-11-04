32a7013c8fd5a90879fe802ee03fec71227dd43c
Lec16sf <Luozy16sf@outlook.com>
2024-11-04 09:21:25 +0800

    update weapon shot for 2 guns, but problems exist for gun2

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,6 +7,8 @@ public class Bullet : Character
     public int endurance = 1; //弹药耐久
     public float speed = 20f;
     public float lifetime = 5f;
+    // 运动方向的向量
+    public Vector3 direction;
 
     public override void Start()
     {
@@ -16,7 +18,11 @@ public class Bullet : Character
 
     public override void FixedUpdate()
     {
-        rb.AddForce(0, 0, speed*Time.deltaTime, ForceMode.VelocityChange);
+        // 沿着运动方向施加力
+        // rb.AddForce(direction * speed * Time.deltaTime, ForceMode.VelocityChange);
+        //使得子弹沿着子弹方向运动
+        rb.velocity = transform.forward * speed;
+        // rb.AddForce(0, 0, speed*Time.deltaTime, ForceMode.VelocityChange);
     }
 
     void OnCollisionEnter(Collision collision)
--- Assets/Scripts/Weapon/AK74.cs
+++ Assets/Scripts/Weapon/AK74.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 namespace Weapon
 {
-    public class Weapon: MonoBehaviour
+    public class AK47: Weapon
     {
-        public float bulletInterval = 0.4f;
-        public GameObject bulletPrefab;
 
         IEnumerator ShootBullets()
         {
@@ -18,26 +16,24 @@ namespace Weapon
             }
         }
         
-        public virtual void Shoot()
+        public override void Shoot()
         {
-            
+            Quaternion rotation = transform.rotation;
+            rotation.x = 0;
+            rotation.y = 0;
+            Instantiate(bulletPrefab, transform.position + new Vector3(0f, 0f, 1.091f), transform.rotation);
         }
         // Start is called before the first frame update
-        public virtual void Start()
+        public override void Start()
         {
-            StartCoroutine(ShootBullets());
+            base.Start();
+            bulletInterval = 0.4f;
         }
 
         // Update is called once per frame
-        public virtual void Update()
+        public override void Update()
         {
-            
+            base.Update();
         }
-        public void OnEnable()
-        {
-            // 每次激活时调用
-            StartCoroutine(ShootBullets());
-        }
-
     }
 }
